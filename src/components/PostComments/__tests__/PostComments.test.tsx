0ad34742775b0020bf1a946188fac6b8a4a7435b
ecblandy <Viny19381@hotmail.com>
2024-08-13 03:21:09 -0300

    Fazendo teste de inserção de comentario

---
+++ src/components/PostComments/__tests__/PostComments.test.tsx
@@ -0,0 +1,25 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Post from '..';
+import PostComment from '..';
+
+describe('Teste para o componente PostComment', () => {
+    it('Deve renderizar o componente corretamente', () => {
+        render(<PostComment/>);
+        expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+
+    it("Deve renderizar um comentario", () => {
+        const {debug} = render(<PostComment/>)
+        
+        fireEvent.change(screen.getByTestId('comment-field'), {
+            target: {
+                value: 'Estudar Jest'
+            }
+        })
+
+        fireEvent.click(screen.getByTestId('btn-comment'))
+        expect(screen.getByText('Estudar Jest')).toBeInTheDocument()
+        debug()
+    })
+});
